09eec96803c40b0619a2d3148bf94db44d4080ea
Joseph Ngandu <joseph.0.ngandu@gmail.com>
2026-03-24 00:05:10 +0200

    Initial Commit

---
+++ Venda/VendaApp.swift
@@ -0,0 +1,17 @@
+//
+//  VendaApp.swift
+//  Venda
+//
+//  Created by Sal🥤 on 3/24/26.
+//
+
+import SwiftUI
+
+@main
+struct VendaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VendaTests/VendaTests.swift
@@ -0,0 +1,17 @@
+//
+//  VendaTests.swift
+//  VendaTests
+//
+//  Created by Sal🥤 on 3/24/26.
+//
+
+import Testing
+@testable import Venda
+
+struct VendaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
